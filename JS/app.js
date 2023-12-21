f4b81a49d2f7d102064782127bdd2d793b7b2a94
OKE225 <120661050+OKE225@users.noreply.github.com>
2023-12-21 23:23:56 +0100

    ⏲️Adding Example HTML, Basic CSS Styles & Script JavaScript For Function To Rendering Timer

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,12 @@
 "use strict";
+const timeSpanEl = document.querySelector("span#time");
+function renderTimer() {
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    const seconds = new Date().getSeconds();
+    timeSpanEl.innerHTML = `${hours} : ${minutes < 10 ? "0" + minutes : minutes} : ${seconds < 10 ? "0" + seconds : seconds}`;
+}
+window.onload = function () {
+    setInterval(renderTimer, 500);
+    renderTimer();
+};
