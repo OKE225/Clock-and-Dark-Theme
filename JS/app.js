b301b1a6648c775f1d7dd32aa578c748b1279f12
OKE225 <120661050+OKE225@users.noreply.github.com>
2023-12-24 10:09:40 +0100

    🌈 Adding Colour To Numbers & Code Refactoring

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,72 @@
 "use strict";
-const timeSpanEl = document.querySelector("span#time");
-function renderTimer() {
+const spanTimer = document.querySelector("span#timer");
+function renderTime() {
     const hours = new Date().getHours();
     const minutes = new Date().getMinutes();
     const seconds = new Date().getSeconds();
-    timeSpanEl.innerHTML = `${hours} : ${minutes < 10 ? "0" + minutes : minutes} : ${seconds < 10 ? "0" + seconds : seconds}`;
+    spanTimer.innerHTML = "";
+    setColorsForNumbers(addBeginningZero(hours), addBeginningZero(minutes), addBeginningZero(seconds));
+}
+function setColorsForNumbers(...args) {
+    args
+        .join(" : ")
+        .split("")
+        .forEach((el) => {
+        const spanEl = document.createElement("span");
+        let color;
+        switch (el) {
+            case "0":
+                color = "#ff595e";
+                break;
+            case "1":
+                color = "#ff924c";
+                break;
+            case "2":
+                color = "#ffca3a";
+                break;
+            case "3":
+                color = "#c5ca30";
+                break;
+            case "4":
+                color = "#8ac926";
+                break;
+            case "5":
+                color = "#52a675";
+                break;
+            case "6":
+                color = "#1982c4";
+                break;
+            case "7":
+                color = "#4267ac";
+                break;
+            case "8":
+                color = "#6a4c93";
+                break;
+            case "9":
+                color = "#b5a6c9";
+                break;
+            case " ":
+                color = "#000000";
+                spanEl.style.width = `${0.5}em`;
+                break;
+            default:
+                color = "#000000";
+                break;
+        }
+        spanEl.innerHTML = el;
+        spanEl.style.color = color;
+        spanTimer.appendChild(spanEl);
+    });
+}
+function addBeginningZero(num) {
+    if (num < 10) {
+        return `0${num}`;
+    }
+    else {
+        return `${num}`;
+    }
 }
 window.onload = function () {
-    setInterval(renderTimer, 500);
-    renderTimer();
+    setInterval(renderTime, 100);
+    renderTime();
 };
--- TS/app.ts
+++ TS/app.ts
@@ -1,18 +1,88 @@
-const timeSpanEl: HTMLSpanElement = document.querySelector(
-  "span#time"
-) as HTMLSpanElement;
+const spanTimer: HTMLDivElement = document.querySelector(
+  "span#timer"
+) as HTMLDivElement;
 
-function renderTimer(): void {
+function renderTime(): void {
   const hours: number = new Date().getHours();
   const minutes: number = new Date().getMinutes();
   const seconds: number = new Date().getSeconds();
 
-  timeSpanEl.innerHTML = `${hours} : ${
-    minutes < 10 ? "0" + minutes : minutes
-  } : ${seconds < 10 ? "0" + seconds : seconds}`;
+  spanTimer.innerHTML = "";
+
+  setColorsForNumbers(
+    addBeginningZero(hours),
+    addBeginningZero(minutes),
+    addBeginningZero(seconds)
+  );
+}
+
+function setColorsForNumbers(...args: string[]): void {
+  args
+    .join(" : ")
+    .split("")
+    .forEach((el: string): void => {
+      const spanEl: HTMLSpanElement = document.createElement(
+        "span"
+      ) as HTMLSpanElement;
+
+      let color: string;
+
+      switch (el) {
+        case "0":
+          color = "#ff595e";
+          break;
+        case "1":
+          color = "#ff924c";
+          break;
+        case "2":
+          color = "#ffca3a";
+          break;
+        case "3":
+          color = "#c5ca30";
+          break;
+        case "4":
+          color = "#8ac926";
+          break;
+        case "5":
+          color = "#52a675";
+          break;
+        case "6":
+          color = "#1982c4";
+          break;
+        case "7":
+          color = "#4267ac";
+          break;
+        case "8":
+          color = "#6a4c93";
+          break;
+        case "9":
+          color = "#b5a6c9";
+          break;
+        case " ":
+          color = "#000000";
+          spanEl.style.width = `${0.5}em`;
+          break;
+        default:
+          color = "#000000";
+          break;
+      }
+
+      spanEl.innerHTML = el;
+      spanEl.style.color = color;
+
+      spanTimer.appendChild(spanEl);
+    });
+}
+
+function addBeginningZero(num: number): string {
+  if (num < 10) {
+    return `0${num}`;
+  } else {
+    return `${num}`;
+  }
 }
 
 window.onload = function (): void {
-  setInterval(renderTimer, 500);
-  renderTimer();
+  setInterval(renderTime, 100);
+  renderTime();
 };
